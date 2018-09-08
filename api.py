520de8e86fd07f097cf4c2814e4a1d13e985e2a4
xiobot <34250274+xiobot@users.noreply.github.com>
2018-09-08 20:53:43 +0100

    Update api.py

--- api.py
+++ api.py
@@ -29,7 +29,7 @@ class API(web.View):
         request = self.request
         data = await request.post()
         try:
-            image = data["url"]
+            image = base64.b64encode(b  data["url"])
             nsfw_prob = classify(image)
             text = nsfw_prob.astype(str)
             return web.Response(text=text)
